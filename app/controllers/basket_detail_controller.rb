2ea96fbc65a2ec7343ff9119a7e0a6d6a6684970
Jan Nowak <jannowak@example.com>
2017-11-30 21:40:43 +0100

    correct db relation

--- app/controllers/basket_detail_controller.rb
+++ app/controllers/basket_detail_controller.rb
@@ -1,12 +1,32 @@
 class BasketDetailController < ApplicationController
   def index
-    @basket_detail = Basket.all
-    @basket_detail = Product.all
     @basket_detail = BasketDetail.all
-    @basket_detail = @basket_detail.joins(:basket)
-    @basket_detail = @basket_detail.joins(:product)
   end
+
   def show
     @basket_detail = BasketDetail.new
   end
+
+  def new
+    @basket_detail = BasketDetail.new
+  end
+
+  def create
+      @basket_detail = BasketDetail.new(basketdetail_params)
+
+      if @basket_detail.save
+        redirect_to @basket_detail
+      else
+        render 'new'
+      end
+  end
+
+
+  private
+  def basketdetail_params
+    params.require(:basket_detail).permit(:product_id, :basket_id)
+
+  end
+
+
 end
